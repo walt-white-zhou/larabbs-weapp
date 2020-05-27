07618e9e94ac7d6766af37df8741c89045d93695
root <root@DESKTOP-6RO2RCV.localdomain>
2020-05-27 17:34:23 +0800

    weapp update user

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,13 @@
-import { request, authRequest } from '@/utils/request'
+import {request, authRequest} from '@/utils/request'
 
 export function getCurrentUser(data) {
   return authRequest('user')
+}
+
+export function updateUser(data) {
+  return authRequest('user', {
+    method: 'put',
+    data: data
+  })
 }
 
