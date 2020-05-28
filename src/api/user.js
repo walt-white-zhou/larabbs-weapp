43a416414e6ba923af185985b06921389627f31a
root <root@DESKTOP-6RO2RCV.localdomain>
2020-05-28 10:13:05 +0800

    update avatar

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,5 @@
 import {request, authRequest} from '@/utils/request'
+import {uploadFile} from '../utils/request'
 
 export function getCurrentUser(data) {
   return authRequest('user')
@@ -8,6 +9,17 @@ export function updateUser(data) {
   return authRequest('user', {
     method: 'put',
     data: data
+  })
+}
+
+export function updateAvatar(avatar) {
+  return uploadFile('images', {
+    method: 'POST',
+    name: 'image',
+    formData: {
+      type: 'avatar'
+    },
+    filePath: avatar
   })
 }
 
